e2e45a03238839fc950bd5a5540cfc2a1d44b1e0
Manon Camille Braint <88578361+manon-uniandes@users.noreply.github.com>
2021-09-06 19:10:53 -0500

    Add files via upload

---
+++ TallerDjango-Vistas-master/measurements/logic/logic_measurements.py
@@ -0,0 +1,17 @@
+from ..models import Measurement
+
+def get_all_measurements():
+    measurements = Measurement.objects.all()
+    return measurements
+
+def get_one_measurement(id, value):
+    measurement = Measurement.objects.get(pk=id)
+    return measurement
+
+def del_one_measurement(id):
+    measurement = Measurement.objects.filter(pk=id).delete()
+    return measurement
+
+def mod_one_measurement(id, entry):
+    measurement = Measurement.objects.select_related().filter(pk=id).update(value=entry)
+    return measurement
